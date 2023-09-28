165879ae8900628c97f064a6dffc316044a8408b
Anita <julia.anitasampaio@gmail.com>
2023-09-28 15:15:31 -0300

    .

--- Aula06/src/NomesDePessoas.java
+++ Aula06/src/NomesDePessoas.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 public class NomesDePessoas {
-    public static void main(String args[]){
+    public static void main(String args[]){{
 
     }
 }

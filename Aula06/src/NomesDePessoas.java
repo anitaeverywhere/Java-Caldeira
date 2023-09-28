4c692b4d9b78a75d04c4bdec71400675906f7cbd
Anita <julia.anitasampaio@gmail.com>
2023-09-28 16:35:04 -0300

    aula de quinta

--- Aula06/src/NomesDePessoas.java
+++ Aula06/src/NomesDePessoas.java
@@ -1,7 +1,26 @@
 import java.util.HashMap;
 import java.util.Map;
 public class NomesDePessoas {
-    public static void main(String args[]){{
+    public static void main(String args[]){
+        Map<String, Integer> nomeIdade = new HashMap<String, Integer>();
+        nomeIdade.put("Andria", 65);
+        nomeIdade.put("Santana", 70);
+        nomeIdade.put("Marco", 16);
+        nomeIdade.put("Maria",17);
+        nomeIdade.put("Matheus",69);
+
+        String NomeDaPessoa = "Matheus";
+        if (nomeIdade.containsKey(NomeDaPessoa)){
+            int idade = nomeIdade.get(NomeDaPessoa);
+            System.out.println(NomeDaPessoa + "tem" + idade);
+        }
+      else {
+            System.out.println(NomeDaPessoa + "Não está no mapa");
+        }
+
+
+
+
 
     }
 }

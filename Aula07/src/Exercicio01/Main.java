0b815af256bea8181bccb8ef41c22559c7d295fe
anitaeverywhere <julia.anitasampaio@gmail.com>
2023-11-25 04:57:23 -0300

    exercise 1

---
+++ Aula07/src/Exercicio01/Main.java
@@ -0,0 +1,11 @@
+package Exercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        Quadrado quadrado = new Quadrado();
+        circulo.desenhar();
+        quadrado.desenhar();
+
+    }
+}
